5f4101c728ba071e1f3501a28cd87d38a755dfe2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-05-04 17:32:39 +0200

    Accept JSON header middleware

---
+++ src/AcceptJsonMiddleware.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Middleware\Json;
+
+use ApiClients\Foundation\Middleware\DefaultPriorityTrait;
+use ApiClients\Foundation\Middleware\ErrorTrait;
+use ApiClients\Foundation\Middleware\MiddlewareInterface;
+use ApiClients\Foundation\Middleware\PostTrait;
+use Psr\Http\Message\RequestInterface;
+use React\Promise\CancellablePromiseInterface;
+use function React\Promise\resolve;
+
+class AcceptJsonMiddleware implements MiddlewareInterface
+{
+    use PostTrait;
+    use ErrorTrait;
+    use DefaultPriorityTrait;
+
+    /**
+     * @param RequestInterface $request
+     * @param array $options
+     * @return CancellablePromiseInterface
+     */
+    public function pre(RequestInterface $request, array $options = []): CancellablePromiseInterface
+    {
+        return resolve($request->withAddedHeader('Accept', 'application/json'));
+    }
+}
